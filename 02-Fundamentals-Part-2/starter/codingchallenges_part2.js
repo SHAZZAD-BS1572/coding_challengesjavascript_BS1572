8b16f63a0b3444b190dc41c5e4c9b23d5185facb
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-05-09 13:05:28 +0600

    part2

--- 02-Fundamentals-Part-2/starter/codingchallenges_part2.js
+++ 02-Fundamentals-Part-2/starter/codingchallenges_part2.js
@@ -1,4 +1,4 @@
-const calcAverage = (a, b, c) => (a + b + c) / 3;
+/*const calcAverage = (a, b, c) => (a + b + c) / 3;
 // console.log(calcAverage(3, 4, 5));
 
 let scoreDolphins = calcAverage(44, 23, 71);
@@ -7,13 +7,39 @@ console.log(scoreDolphins, scoreKoalas);
 
 const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
   } else if (avgKoalas >= 2 * avgDolphins) {
-    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
   } else {
     console.log("No team wins...");
   }
 };
-// checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
 
-checkWinner(576, 111);
+//Exercise array
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const age = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(age);*/
+
+//Part 2 // Coding Challenge #2 tip calculator
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const bill = [125, 555, 44];
+const tips = [calcTip(bill[0]), calcTip(bill[1]), calcTip(bill[2])];
+const totals = [bill[0] + tips[0], bill[1] + tips[1], bill[2] + tips[2]];
+
+console.log(`Bills ${bill}, Tips ${tips}, Totals ${totals}`);
